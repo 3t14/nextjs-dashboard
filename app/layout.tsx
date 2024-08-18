dc4b5ff23c049be96d93b9e74d91dc76881c0d6d
Michiru Tanaka <michiru@r4hs.com>
2024-08-18 04:26:22 +0000

    Initial Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import '@/app/ui/global.css';
+import { inter, notoSansJP } from '@/app/ui/fonts';
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ja">
+      <body className={`${notoSansJP.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+import { Noto_Sans_JP } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'] });
+export const notoSansJP = Noto_Sans_JP({ subsets: ['latin'] }); 
